f530c75123f7b117269f434a5310b2643c4dd7f4
Evandro Florscuk <evandro_ff@me.com>
2023-06-19 23:11:42 -0300

    Project Updated (frontend) => Search Cep component created

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,8 +1,12 @@
 import styles from "./Home.module.css"
 
 const Home = _ => {
-    return(
-        <>Home</>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao SEARCH-CEP</h1>
+            <button>Teste 1</button>
+            <button>Teste 2</button>
+        </section>
     )
 }
 
---
+++ frontend/src/components/pages/SearchNumber.js
@@ -0,0 +1,9 @@
+const SearchNumber = () => {
+    return(
+    <>
+        <h1>Digite o número do CEP para consulta</h1>
+        <input type="text"></input>
+    </>)
+}
+
+export default SearchNumber
