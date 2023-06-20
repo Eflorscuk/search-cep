3058d0c6bcf3296bc66c0aa70e30c1fd3c6d38b0
Evandro Florscuk <evandro_ff@me.com>
2023-06-20 16:53:31 -0300

    Project Updated (backend) => route fixed

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
-const searchRouter = require('./routes/search')
+const search = require('./routes/search')
+const axios = require('axios')
 
 const app = express()
 const port = 8084
 
-app.use('/search', searchRouter)
+app.use(search)
 
 app.listen(port, _ => console.log(`Programa executando na porta ${port}`))
--- backend/routes/search.js
+++ backend/routes/search.js
@@ -3,14 +3,15 @@ const axios = require('axios')
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-    try {
+router.get('/myroute', async (req, res) => {
+  try {
+      console.log('====> ', req)
       const response = await axios.get('https://viacep.com.br/ws/01001000/json/');
       const data = response.data;
       res.json(data);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+      res.status(500).json({ error: 'Erro do Servidor' });
     }
 })
 
